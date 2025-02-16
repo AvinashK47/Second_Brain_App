dbb5c2344c38b28062f50074df50877ca2490d9e
Avinash Kushwaha <avinash.kushwaha1501@gmail.com>
2025-02-16 17:08:38 +0530

    2nd Commit

---
+++ src/db.ts
@@ -0,0 +1,25 @@
+import mongoose,{connect, model,Schema} from "mongoose"
+
+export async function ConnectDB() {
+    try {
+       await connect('mongodb+srv://avinash:password@123@cluster0.i8r3m.mongodb.net/');
+        console.log("Connected to Database!!!");
+    }
+    catch(err) {
+        console.log("Mongoose Connection Error : ", err);
+    }
+}
+
+export const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique :true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+export const UserModel = model('User', UserSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,48 @@
-import express from 'express';
+import express, {Request,Response,RequestHandler} from 'express';
+import { ConnectDB, UserModel } from './db';
 
 const app = express();
 
-app.post('/api/v1/signup', (req, res) => {
+ConnectDB();
+
+app.use(express.json());
+
+app.post('/api/v1/signup',async (req:Request,res:Response) => {
     
+    const username: string = req.body.username;
+    const password: string | number = req.body.password;
+
+    try {
+            
+        const existingUser = await UserModel.findOne({ username });
+        
+        if (existingUser) {
+            return res.status(403).json({
+                message: "User already exists with this username"
+            });
+        }
+        else {
+            await UserModel.create({
+                username: username,
+                password: password
+            })
+            return res.json({
+                message: "User created Successfully"
+            });
+                
+        }
+    }
+    catch(err) {
+        console.log(err);
+        return res.status(500).json({ message: "Internal Server error" });
+    }
+})
+
+app.post('/api/v1/signin', (req, res) => {
+    
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is running at port 3000")
 })
